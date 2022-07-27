adfe89425cc455e7e1fa03b4cc335b493c01e457
Hala <halqadi@bloovo.com>
2022-07-27 14:02:29 +0300

    some material components

--- material/src/app/app.component.ts
+++ material/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'material';
+export class AppComponent implements OnInit {
+    nameControl = new FormControl('');
+    nameControl2 = new FormControl('');
+    options: string[] = ['One', 'Two', 'Three'];
+    filteredOptions!: Observable<string[]>;
+    hidden = true;
+    fontStyle = '';
+    fontStyleControl = new FormControl('');
+    longText = 'text text text text text text text text text text text text';
+
+    ngOnInit(): void {
+        this.filteredOptions = this.nameControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this.filter(value)),
+        );
+    }
+
+    filter(value: string): string[] {
+        return this.options
+            .filter(option => option.toLowerCase().includes(value.toLowerCase()));
+    }
+
+    toggleBadge(): void {
+        this.hidden = !this.hidden;
+    }
 }
--- material/src/app/app.module.ts
+++ material/src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
     declarations: [
@@ -15,6 +26,18 @@ import { MatSliderModule } from '@angular/material/slider';
         AppRoutingModule,
         BrowserAnimationsModule,
         MatSliderModule,
+        ReactiveFormsModule,
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatBadgeModule,
+        MatButtonModule,
+        MatIconModule,
+        MatButtonToggleModule,
+        FormsModule,
+        MatCardModule,
+        MatProgressBarModule,
+        MatDividerModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
